066743fcea22b296d04d907fa2de3a46fc78afb8
Chereji Iulia <72073380+912-Chereji-Iulia@users.noreply.github.com>
2022-11-16 08:39:43 +0200

    lab7- final FA

---
+++ lab7/main.py
@@ -0,0 +1,12 @@
+from FiniteAutomata import FiniteAutomata
+from Scanner import Scanner
+
+if __name__ == '__main__':
+    scanner = Scanner()
+    st, pif, message = scanner.scan("input/Lab1a - p3.txt")
+    fa = FiniteAutomata('input/constant-integer.in')
+    fa.start()
+
+
+
+
